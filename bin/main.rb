edba81f978a6f0855a76da5032c03e0f29b28b53
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-07-27 14:45:30 +0800

    Linter error fix

--- bin/main.rb
+++ bin/main.rb
@@ -1,26 +1,28 @@
 #!/usr/bin/env ruby
+require 'matrix'
 
 class Interface
   require 'io/console'
-
-  @@move_range = [1,2,3,4,5,6,7,8,9] # TODO: Transfer to game logic in Milestone 3
-  @@move_map = { 1=>0, 2=>2, 3=>4, 4=>0, 5=>2, 6=>4, 7=>0, 8=>2, 9=>4 } 
   
-  def initialize           
-    @turn_count = 0     
-    @next_player = 2    
-    @grid = ["_|_|_",
-             "_|_|_",
-             " | | "]
+  
+  def initialize
+    @move_range = [1, 2, 3, 4, 5, 6, 7, 8, 9] # TODO: Transfer to game logic in Milestone 3
+    @move_map = { 1 => 0, 2 => 2, 3 => 4, 4 => 0, 5 => 2, 6 => 4, 7 => 0, 8 => 2, 9 => 4 }     
 
-    @control_grid = ["1|2|3",
-                     "4|5|6",
-                     "7|8|9"]
+    @turn_count = 0
+    @player = 2   
+    @grid = ['_|_|_',
+             '_|_|_',
+             ' | | ']
+
+    @control_grid = ['1|2|3',
+                     '4|5|6',
+                     '7|8|9']
     
     print_introduction
-    display_game        
-    display_controls        
-    puts "Player 1 please enter your first move (or press q + enter to exit game):"
+    display_game    
+    display_controls
+    puts 'Player 1 please enter your first move (or press q + enter to exit game):'
     play
   end
 
@@ -34,12 +36,13 @@ class Interface
   end
 
   def display_game    
-    @grid.each {|row| puts row}    
+    @grid.each { |row| puts row }    
   end
 
   def display_controls    
-    puts "\nEnter a number from 1 to 9 to place your marker on the game grid above. The grid below shows where your marker will be placed if you enter a number."    
-    @control_grid.each {|row| puts row}    
+    puts "\nEnter a number from 1 to 9 to place your marker on the game grid above.\n"
+    puts 'The grid below shows where your marker will be placed if you enter a number.'
+    @control_grid.each { |row| puts row }
   end
   
   def get_input
@@ -50,15 +53,15 @@ class Interface
     
     move = get_input
     move_int = move.to_i    
-    if @@move_range.include? move_int
+    if @move_range.include? move_int
       puts "Previous move: #{move}"
-      puts "Turn: #{@turn_count}, Player #{@next_player.to_s} is up next."
+      puts "Turn: #{@turn_count}, Player #{@player.to_s} is up next."
       if move_int < 4
-        @grid[0][@@move_map[move_int]] = player_symbol        
+        @grid[0][@move_map[move_int]] = player_symbol        
       elsif move_int > 3 and move_int < 7
-        @grid[1][@@move_map[move_int]] = player_symbol        
+        @grid[1][@move_map[move_int]] = player_symbol        
       else
-        @grid[2][@@move_map[move_int]] = player_symbol                      
+        @grid[2][@move_map[move_int]] = player_symbol                      
       end
     elsif move == 'q'
       puts 'Exiting game... See you next time!'
@@ -71,11 +74,11 @@ class Interface
   end
 
   def player_symbol
-    @turn_count.even? ? "x" : "o"
+    @turn_count.even? ? 'x' : 'o'
   end
 
   def next_player
-    player_symbol == "x" ? 2 : 1    
+    player_symbol == 'x' ? 2 : 1    
   end
 
   def p2_move
@@ -85,20 +88,48 @@ class Interface
     while @turn_count < 9
       player_move
       display_game
-      puts "Please enter your next move:"
-      # TODO for game logic: if win condition == True 
+      puts 'Please enter your next move:'
+      # TODO for game logic: if win condition == True
     end
   end
 
   def win
-    # game logic 
+    # game logic
   end
 
   def display_rules
     # for later
   end
 
-
 end
 
 
+#need something that stores move history
+
+# there's something here - were we add to rows and columns 
+
+# ruby has a matrix class - with diagonal - might make things easier over-all
+
+# row - win
+# 1,2,3
+# 4,5,6 
+# 7,8,9
+
+# col - win always + 3
+# 1,4,7
+# 2,5,8
+# 3,6,9
+
+# diagonal 
+# 1,5,9 + 4
+# 3,5,7 + 2
+
+# how to store 
+
+g = Interface.new
+
+
+
+
+
+#g = Interface.new
