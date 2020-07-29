0ff816bb41746b9ee760901b751bf683b149fa5b
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-07-29 15:42:15 +0800

    FIX linter issue

--- bin/main.rb
+++ bin/main.rb
@@ -50,6 +50,7 @@ class Interface < Game
       puts "Player #{next_player} you're up next."
       puts 'Please enter your next move (or press q + enter to quit):'
       next unless @game[:win] == true
+
       puts 'We have a winner!'
       puts "Player #{current_player} Wins!"
       show_game
