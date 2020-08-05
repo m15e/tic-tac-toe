649186da1d29a297e79f7bbf0998e4503778d1cc
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-08-05 20:45:40 +0800

    ADD tests for player symbol

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -1,2 +1,26 @@
 require 'logic'
 
+describe Game do
+  
+  describe '#player_symbol' do
+    it 'returns \'x\' if turn count is 0' do
+      game = Game.new      
+      expect(game.player_symbol).to eql('x')
+    end
+
+    it 'returns \'o\' if turn count is 1' do
+      game = Game.new
+      game.player_move(1)
+      expect(game.player_symbol).to eql('o')
+    end
+
+    it 'return \'x\' if turn count is 2' do
+      game = Game.new
+      game.player_move(1)
+      game.player_move(4)
+      expect(game.player_symbol).to eql('x')
+    end
+
+  end
+end
+
