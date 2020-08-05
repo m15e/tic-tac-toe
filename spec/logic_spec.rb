1a274ea393dbaa700e82e5e7937fcf4bf35ca73f
Clayton Siby <claytonsiby@gmail.com>
2020-08-05 17:07:34 +0200

    fix linter errors

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -2,18 +2,18 @@ require 'logic'
 
 describe Game do
   let(:game) { Game.new }
-  
+
   describe '#player_symbol' do
-    it 'returns \'x\' if turn count is 0' do      
+    it 'returns \'x\' if turn count is 0' do
       expect(game.player_symbol).to eql('x')
     end
 
-    it 'returns \'o\' if turn count is 1' do      
+    it 'returns \'o\' if turn count is 1' do
       game.player_move(1)
       expect(game.player_symbol).to eql('o')
     end
 
-    it 'returns \'x\' if turn count is 2' do      
+    it 'returns \'x\' if turn count is 2' do
       game.player_move(1)
       game.player_move(4)
       expect(game.player_symbol).to eql('x')
@@ -21,14 +21,14 @@ describe Game do
   end
 
   describe '#next_player' do
-    it 'returns 2 if player_symbol is \'x\'' do      
+    it 'returns 2 if player_symbol is \'x\'' do
       game.player_move(9)
       expect(game.next_player).to eql(2)
     end
   end
 
   describe '#current_player' do
-    it 'returns 1 if next_player returns 2' do      
+    it 'returns 1 if next_player returns 2' do
       game.player_move(9)
       expect(game.current_player).to eql(1)
     end
@@ -69,4 +69,3 @@ describe Game do
     end
   end
 end
-
