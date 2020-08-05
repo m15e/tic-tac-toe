5de20131d0124269832018f0a9a84869571c52a9
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-08-05 20:52:10 +0800

    ADD tests for current_player and next_player

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -14,13 +14,28 @@ describe Game do
       expect(game.player_symbol).to eql('o')
     end
 
-    it 'return \'x\' if turn count is 2' do
+    it 'returns \'x\' if turn count is 2' do
       game = Game.new
       game.player_move(1)
       game.player_move(4)
       expect(game.player_symbol).to eql('x')
     end
+  end
 
+  describe '#next_player' do
+    it 'returns 2 if player_symbol is \'x\'' do
+      game = Game.new
+      game.player_move(9)
+      expect(game.next_player).to eql(2)
+    end
+  end
+
+  describe '#current_player' do
+    it 'returns 1 if next_player returns 2' do
+      game = Game.new
+      game.player_move(9)
+      expect(game.current_player).to eql(1)
+    end
   end
 end
 
